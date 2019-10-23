470580e053f961494c257d0e9825c7137efe593a
Anna Yablunovskaya <lad.shada@gmail.com>
2019-10-24 02:18:36 +0300

    start from scratch

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import logging
 import os
-from telegram import ReplyKeyboardMarkup
+from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, \
+                    InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from flask import Flask, redirect
+from google_auth_oauthlib.flow import Flow
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
@@ -13,9 +15,19 @@ app = Flask(__name__)
 
 
 def start(bot, update):
-    text = 'Вызван /start'
-    my_keyboard = ReplyKeyboardMarkup([['Расписание']])
-    update.message.reply_text(text, reply_markup=my_keyboard)
+    my_keyboard = ReplyKeyboardMarkup([
+                                    ['Расписание'], ['Создать мероприятие']]
+                                    )
+    update.message.reply_text(reply_markup=my_keyboard)
+
+
+def google_auth(bot, update):
+    auth_url = f"https://{environ.get('HEROKU_APP_NAME')}.herokuapp.com/login/"
+    keyboard = [
+                [InlineKeyboardButton('Нажми на ссылку, чтобы авторизоваться в гугле', url=auth_url)]
+                ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text('Авторизоваться в google', reply_markup=reply_markup)
 
 
 def help(bot, update):
@@ -29,12 +41,27 @@ def message(bot, update):
                 )
     update.message.reply_text(user_text)
 
+# def google_auth_flow():
+#     scopes = ["https://www.googleapis.com/auth/calendar"]
+#     client_config_data = {
+#         "web": {
+#                 "client_id": os.environ.get('GOOGLE_CLIENT_ID'),
+#                 "project_id": os.environ.get('GOOGLE_PROJECT_ID'),
+#                 "auth_uri": os.environ.get('GOOGLE_AUTH_URI'),
+#                 "token_uri": os.environ.get('GOOGLE_TOKEN_URI'),
+#                 "auth_provider_x509_cert_url": os.environ.get('GOOGLE_AUTH_CERT_URI'),
+#                 "client_secret": os.environ.get('GOOGLE_CLIENT_SECRET')}}
+#     flow = Flow.from_client_config(
+#         client_config_data,
+#         scopes=scopes,
+#         redirect_uri=os.environ.get("GOOGLE_REDIRECT_URI"))   
 
 def main():
     mybot = Updater(TOKEN)
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler('google_auth', google_auth))
     dp.add_handler(MessageHandler(Filters.text, message))
 
     mybot.start_polling()
@@ -44,6 +71,28 @@ def main():
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return redirect(TELEGRAM_BOT_URL, code=302)
+
+
+@app.route('/login')
+def login():
+    scopes = ["https://www.googleapis.com/auth/calendar"]
+    client_config_data = {
+        "web": {
+                "client_id": os.environ.get('GOOGLE_CLIENT_ID'),
+                "project_id": os.environ.get('GOOGLE_PROJECT_ID'),
+                "auth_uri": os.environ.get('GOOGLE_AUTH_URI'),
+                "token_uri": os.environ.get('GOOGLE_TOKEN_URI'),
+                "auth_provider_x509_cert_url": os.environ.get('GOOGLE_AUTH_CERT_URI'),
+                "client_secret": os.environ.get('GOOGLE_CLIENT_SECRET')}}
+    flow = Flow.from_client_config(
+        client_config_data,
+        scopes=scopes,
+        redirect_uri=os.environ.get("GOOGLE_REDIRECT_URI"))
+    auth_url, _ = flow.authorization_url(prompt='consent')
+    google_code = input('Введите код авторизации: ')
+    flow.fetch_token(code=google_code)
+    session = flow.authorized_session()
+    print(session.get('https://www.googleapis.com/userinfo/v2/me').json())
 
 
 if __name__ == "__main__":
