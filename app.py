183e098f4dca28f26c7da348cada086d1056fa5a
Anna Yablunovskaya <lad.shada@gmail.com>
2019-10-23 19:51:44 +0300

    added debug data

--- app.py
+++ app.py
@@ -15,8 +15,10 @@ logger = logging.getLogger(__name__)
 
 TOKEN = environ.get('TELEGRAM_BOT_TOKEN')
 URL = "https://{}.herokuapp.com/".format(environ.get('HEROKU_APP_NAME'))
-
+logger.info(TOKEN)
+print(TOKEN)
 bot = Bot(TOKEN)
+print(bot.__dir__)
 update_queue = Queue()
 dp = Dispatcher(bot, update_queue)
 app = Flask(__name__)
