92ef548c9acfc0225007a3b73f8f7e7780b11b0c
Anna Yablunovskaya <lad.shada@gmail.com>
2019-10-28 23:04:25 +0300

    check out what is in context

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def start(update, context):
 
 
 def google_auth(update, context):
-    print(context)
+    print(context.user_data)
     auth_url = f"https://{os.environ.get('HEROKU_APP_NAME')}.herokuapp.com/authorize/"
     keyboard = [
                 [InlineKeyboardButton('Нажми на ссылку, чтобы авторизоваться в гугле', url=auth_url)]
