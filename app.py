bb8535c277965f7c0cbda3778c6ecbfbe4523e5e
Anna Yablunovskaya <lad.shada@gmail.com>
2019-10-29 20:58:54 +0300

    added calendar list

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def google_set_default_calendar(update, context):
         keyboard = []
         for item in calendars['items']:
             print(item['summary'])
-            keyboard.append([InlineKeyboardButton(item['summary'], callback_data='got it'])
+            keyboard.append([InlineKeyboardButton(item['summary'], callback_data='got it')])
         reply_markup = InlineKeyboardMarkup(keyboard)
         update.message.reply_text('Выбери календарь, который хочешь назначить по умолчанию', reply_markup=reply_markup)
 
