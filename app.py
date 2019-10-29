8e2df89b161ca216dba634c2225543cd4655e673
Anna Yablunovskaya <lad.shada@gmail.com>
2019-10-29 15:16:49 +0300

    add agenda

--- app.py
+++ app.py
@@ -83,8 +83,8 @@ def credentials_to_dict(credentials):
 def is_authorized(user_id):
     print("ID: ", user_id)
     user_str_id = str(user_id)
-    user_creds = mongo.db.google_credentials.find_one({'_id': user_id)})
-    
+    user_creds = mongo.db.google_credentials.find_one({'_id': user_str_id)})
+
 
 
 def start(update, context):
