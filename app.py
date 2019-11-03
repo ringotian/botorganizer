4605f8f4f3ed5ed8869a0e24915edd9859ab8fb5
Anna Yablunovskaya <lad.shada@yandex.ru>
2019-11-03 20:26:13 +0300

    clean flask app for heroku

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
-import webapp
+from webapp import create_app
 
+if __name__ == "__main__":
+    app = create_app()
--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,12 +1,9 @@
 from flask import Flask, redirect
-from webapp import blueprint as auth_blueprint
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
-    app.config['MONGO_URI'] = app.config['MONGODB_URI']+'?retryWrites=false'
-    app.register_blueprint(auth_blueprint)
 
     @app.route('/')
     def index():
