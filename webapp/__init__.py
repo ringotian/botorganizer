2a3728f43e710236113947b27f38d405e997b499
Anna Yablunovskaya <lad.shada@yandex.ru>
2019-11-03 22:27:44 +0300

    add auth blueprint

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask, redirect
-
+from webapp.auth.views import blueprint as auth_blueprint
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
-
+    app.register_blueprint(auth_blueprint)
     @app.route('/')
     def index():
         return redirect(app.config['TELEGRAM_BOT_URL'])
