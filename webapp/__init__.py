91684b815adf1a2c02f46c8d8104a80771ae5500
Anna Yablunovskaya <lad.shada@gmail.com>
2019-11-04 22:00:51 +0300

    fix mongo creation

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from flask_pymongo import PyMongo
+
+mongo = PyMongo()
 from webapp.auth.routes import blueprint as auth_blueprint
 from webapp.main.routes import blueprint as main_blueprint
-from webapp.db import mongo
 
 
 def create_app():
     app = Flask(__name__)
-    mongo.init_app(app)
     app.config.from_pyfile('config.py')
+
+    mongo.init_app(app)
     app.register_blueprint(auth_blueprint)
     app.register_blueprint(main_blueprint)
     return app
--- webapp/db.py
+++ webapp/db.py
@@ -1,7 +1,4 @@
-from flask_pymongo import PyMongo
-
-
-mongo = PyMongo()
+from webapp import mongo
 
 
 def get_db():
